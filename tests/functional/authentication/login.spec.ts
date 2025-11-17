b2262d699789fea58b2dcac007510b53ccaaac4a
Gérald Leban <gerald.leban0@gmail.com>
2025-11-17 06:57:33 +0100

    test: add empty functional test for registration and login

---
+++ tests/functional/authentication/login.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '@japa/runner'
+
+test.group('Authentication login', () => {
+  test('example test', async ({ assert }) => {
+  })
+})
---
+++ tests/functional/authentication/register.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '@japa/runner'
+
+test.group('Authentication register', () => {
+  test('example test', async ({ assert }) => {
+  })
+})
